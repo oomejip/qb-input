fc2095d0bf2ca662585c6292e742d0b4cab44e90
VKTDEVELOPMENT <164358696+Valkata4235421@users.noreply.github.com>
2024-10-17 22:16:56 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'VKT DEVELOPMENT'
+description 'qb-input design by VKT DEVELOPMENT'
+version '1.0.0'
+
+client_scripts {
+    'client/*.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/styles/*.css',
+    'html/script.js'
+}
